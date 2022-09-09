97fd5efa3e0869421466db40dcc093fbef5eff6b
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2022-09-09 20:08:09 -0300

    created container deploy workflow

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 const commonConfig = require('./webpack.common');
 
@@ -21,9 +21,6 @@ const devConfig = {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js'
       },
       shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html'
     }),
   ],
 };
