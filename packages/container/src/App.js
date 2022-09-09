c20269a6eabb0682544c884bd9f092f41fd283b3
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2022-09-09 20:55:41 -0300

    Update App.js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default function AppEntry() {
+   console.log('ldev::AppEntry - Appl loaded');
+  
   return <div>
     <h1>Hi there!</h1>
     <hr />
